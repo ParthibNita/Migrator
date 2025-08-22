c907f5645f55f3c05a0bc8f186011bf8c229e2f2
Parthib Deb <debparthib0040@gmail.com>
2025-08-22 22:52:32 +0530

    Add MongoDB connection and required dependencies

--- server/index.js
+++ server/index.js
@@ -1,25 +1,33 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import connectDB from './db/index.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 // --- API Routes --- //
 import spotifyRoutes from './routes/spotify.routes.js';
 app.use('/api/spotify', spotifyRoutes);
 
-app.get('/api/test', (req, res) => {
-  res.json({
-    message: `Hello from the server! ✨ The time is ${new Date().toLocaleTimeString()}`,
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to the database', err);
   });
-});
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
-});
