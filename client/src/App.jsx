b799c41538b6ae4ccfdda1c74165926212b804ab
Parthib Deb <debparthib0040@gmail.com>
2025-08-24 00:22:02 +0530

    Refactor App component to use useAuth hook for authentication and loading state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,23 @@
 import { useState, useEffect } from 'react';
 import { LoginPage } from './Pages/LoginPage.jsx';
 import { DashboardPage } from './Pages/DashboardPage.jsx';
+import { useAuth } from './hooks/useAuth.js';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
-  const [accessToken, setAccessToken] = useState(
-    localStorage.getItem('spotify_accesstoken')
-  );
+  const { accessToken, loading, user } = useAuth();
 
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get('access_token');
-    const expiry = urlParams.get('expires_in');
-
-    if (token && expiry) {
-      setAccessToken(token);
-      localStorage.setItem('spotify_accesstoken', token);
-      localStorage.setItem(
-        'spotify_expiry',
-        Date.now() + parseInt(expiry) * 1000
-      );
-      window.history.pushState({}, document.title, '/');
-    }
-  }, []);
-
+  if (loading) {
+    return (
+      <div className="min-h-screen bg-neutral-900 flex items-center justify-center">
+        <p className="text-cyan-600">Loading...</p>
+      </div>
+    );
+  }
   return (
-    <div className="App text-center p-4">
-      {accessToken ? (
-        <DashboardPage accessToken={accessToken} />
-      ) : (
-        <LoginPage />
-      )}
+    <div className="App bg-neutral-900 min-h-screen text-white">
+      {user && <Navbar />}
+      {accessToken ? <DashboardPage /> : <LoginPage />}
     </div>
   );
 }
