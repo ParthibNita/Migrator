030dd0514b1be417b7cb2a87dbad9750f93a682d
Parthib Deb <debparthib0040@gmail.com>
2025-08-24 00:21:43 +0530

    Add Navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import { useAuth } from './../hooks/useAuth.js';
+import { AvatarFallback, Avatar, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+const Navbar = () => {
+  const { user, logout } = useAuth();
+  //   console.log('User in Navbar:', user);
+
+  if (!user) return null;
+
+  return (
+    <nav className="bg-neutral-950 text-cyan-950 p-4 flex justify-between items-center">
+      <div className="text-xl font-bold">Migrator</div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="relative h-10 w-10 rounded-full">
+            <Avatar>
+              <AvatarImage src={user.images?.[0]?.url} alt={user.displayName} />
+              <AvatarFallback>{user.displayName?.[0]}</AvatarFallback>
+            </Avatar>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">
+                {user.displayName}
+              </p>
+              <p className="text-xs leading-none text-muted-foreground">
+                {user.email}
+              </p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={logout}>Log out</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </nav>
+  );
+};
+
+export default Navbar;
