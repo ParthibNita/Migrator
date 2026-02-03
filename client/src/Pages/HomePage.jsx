ac03f35104cf4f167da33fa00d3c282c0d94cb78
Parthib Deb <debparthib0040@gmail.com>
2026-02-03 15:19:50 +0530

    fix slight error

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Button } from '@/components/ui/button';
-import useAuthStore from '../store/authStore';
+import useAuthStore from '../store/AuthStore.js';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { Play, ArrowRight, Music, Youtube, Sparkles } from 'lucide-react';
